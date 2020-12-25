90bce01bd122df14ad6f19ccb609ab9d2d900326
fiberone <andrewbrucelucero@gmail.com>
2020-12-25 01:19:47 -0500

    Update plotutil.lua

--- plotutil.lua
+++ plotutil.lua
@@ -1,7 +1,27 @@
--- https://stackoverflow.com/questions/12161277/how-to-rotate-a-vertex-around-a-certain-point
-function rotatePoints(ang, center, ...)
+function getCenterPoint(...)
+    local minx, miny, maxx, maxy = 0,0,0,0
+    local x = {}
+    local y = {}
     local points = {...}
+    for i, point in pairs(points) do
+        table.insert(x, point.x)
+        table.insert(y, point.y)
+    end
+    maxx = math.max(table.unpack(x))
+    minx = math.min(table.unpack(x))
+    maxy = math.max(table.unpack(y))
+    miny = math.min(table.unpack(y))
+    xy = {x=(maxx+minx)/2, y=(maxy-miny)/2}
+    return xy
+end
+
+-- https://stackoverflow.com/questions/12161277/how-to-rotate-a-vertex-around-a-certain-point
+function rotatePoints(ang, center, poly)
+    local points = {}
     local newPoints = {}
+    for i=1, poly.getPointCount, 1 do
+        table.insert(points, table.pack(poly.getPoint(i)))
+    end
     --local shifted = {} 
     --for i, point in pairs(points) do
     --    table.insert(shifted, {point.x-center.x, point.y-center.y})
@@ -11,7 +31,7 @@ function rotatePoints(ang, center, ...)
         --r = math.sqrt((point.x-center.x)^2+(point.y-center.y)^2)
         local ptx = center.x + ( math.cos(ang) * (point.x-center.x) + math.sin(ang) * (point.y -center.y))
         local pty = center.y + ( -math.sin(ang) * (point.x-center.x) + math.cos(ang) * (point.y -center.y))
-        table.insert(newPoints, {x=ptx, y=pty})
+        poly.setPoint(i, ptx, pty)
     end
     return newPoints
 end
